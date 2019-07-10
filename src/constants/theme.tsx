cb503ca352dec5eda61c054e8a8bb1d299e2df1c
shunsuke_sakurai <shunsuke_sakurai@gmail.com>
2019-07-10 11:05:43 +0900

    feat: Add theme file

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -1,33 +1,19 @@
-export const theme = {
-  breakpoints: ['750px', '1000px', '1440px'],
-  zIndices: {
-    spMenu: 100000
-  },
-  fonts: {
-    yuMincho:
-      '"游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "Sawarabi Mincho", "MS P明朝", "MS PMincho", serif',
-    yuGothic:
-      '"游ゴシック体", "Yu Gothic", YuGothic, "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, "MS Pゴシック", "MS PGothic", sans-serif',
-    crimsonText: '"Crimson Text", serif',
-    oswald:
-      '"Oswald", "游ゴシック体", "Yu Gothic", YuGothic, "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, "MS Pゴシック", "MS PGothic", sans-serif'
-  },
-  baseWidth: {
-    pc: '1440px',
-    sp: '375px'
-  },
+const theme = {
+  borderRadius: '5px',
   colors: {
-    green: '#556b4b',
-    'green-light': '#697f5f',
-    gold: '#c9bc9c',
-    'alert-gold': '#cc9444',
-    'letter-black': '#282828',
-    'letter-black-light': '#505050',
-    gray: '#c8c8c8',
-    'gray-light': '#e0e0e0',
-    'background-gray': '#f4f4f4',
-    white: '#fff',
-    red: '#ce5261',
-    'dull-orange': '#cc9444'
+    charcoalGrey: '#292f33',
+    charcoalLightGrey: '#3e474d',
+    paleGrey: '#f4f8fa',
+    paleLightGrey: '#fafbfc',
+    iceBlue: '#eaf1f5',
+    veryLightBlue: '#e3edf3',
+    burntOrange: '#c53300',
+    pinkRed: '#ff0061',
+    kermitGreen: '#4aac00',
+    kermitGreenTwo: '#55c500',
+    blueGrey: '#879399',
+    lightGreyBlue: '#a8b7bf'
   }
 };
+
+export default theme;
